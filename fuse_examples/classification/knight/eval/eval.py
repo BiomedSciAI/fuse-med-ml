9b43142df0f6efa23854a3616a23d89b0d4a7ee5
Alex Golts <alex.golts@ibm.com>
2022-01-16 15:50:02 +0200

    revert to default eval input arguments

--- fuse_examples/classification/knight/eval/eval.py
+++ fuse_examples/classification/knight/eval/eval.py
@@ -241,10 +241,10 @@ if __name__ == "__main__":
     """
     if len(sys.argv) == 1:
         # no arguments - set arguments inline - see details in function eval()
-        target_filename = "example2/example_targets.csv"
-        task1_prediction_filename = None
-        task2_prediction_filename = "example2/example_task2_predictions.csv"
-        output_dir = "example2/results"
+        target_filename = "example/example_targets.csv"
+        task1_prediction_filename = "example/example_task1_predictions.csv"
+        task2_prediction_filename = "example/example_task2_predictions.csv"
+        output_dir = "example/results"
     else:
         # get arguments from sys.argv
         assert len(sys.argv) == 5, f'Error: expecting 4 input arguments, but got {len(sys.argv)-1}. Usage: python eval.py <target_filename> <task1_prediction_filename> <task2_prediction_filename> <output_dir>. See details in function eval()'
