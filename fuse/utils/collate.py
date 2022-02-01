a2f411e0150f1e6125b4e3b1ce739e3d5d34930d
moshiko <moshiko.raboh@ibm.com>
2022-02-01 10:00:06 +0200

    integrate eval package

---
+++ fuse/utils/collate.py
@@ -0,0 +1,95 @@
+"""
+(C) Copyright 2021 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Created on June 30, 2021
+
+"""
+from typing import Any, Callable, Dict, List, Sequence, Tuple
+
+from fuse.utils import NDict
+
+class CollateToBatchList(Callable):
+    """
+    Collate list of dictionaries to a batch dictionary, each value in dict will be a list of values collected from all samples.
+    """
+    def __init__(self, skip_keys: Sequence[str]=tuple(), raise_error_key_missing: bool = True, special_handlers_keys: Dict[str, Callable] = None):
+        """
+        :param skip_keys: do not collect the listed keys
+        :param raise_error_key_missing: if False, will not raise an error if there are keys that do not exist in some of the samples. Instead will set those values to None.
+        """
+        self._skip_keys = skip_keys
+        self._raise_error_key_missing = raise_error_key_missing
+        
+    def __call__(self, samples: List[Dict]) -> Dict:
+        """
+        collate list of samples into batch_dict
+        :param samples: list of samples
+        :return: batch_dict
+        """
+        batch_dict = NDict()
+        
+        # collect all keys
+        keys = self._collect_all_keys(samples)
+        
+        # collect values
+        for key in keys:
+
+            # skip keys
+            if key in self._skip_keys:
+                continue
+            
+            try:
+                # collect values into a list
+                collected_values, has_error = self._collect_values_to_list(samples, key)
+                batch_dict[key] = collected_values
+            except:
+                print(f'Error: Failed to collect key {key}')
+                raise
+
+        return batch_dict
+
+    def _collect_all_keys(self, samples: List[Dict]):
+        """
+        collect list of keys used in any one of the samples
+        :param samples: list of samples
+        :return: list of keys
+        """
+        keys = set()
+        for sample in samples:
+            if not isinstance(sample, NDict):
+                sample = NDict(sample)
+            keys |= set(sample.keypaths())
+        return keys
+
+    def _collect_values_to_list(self, samples: List[str], key: str) -> Tuple[List, bool]:
+        """
+        collect values of given key into a list 
+        :param samples: list of samples
+        :param key: key to collect
+        :return: list of values
+        """
+        has_error = False
+        collected_values = []
+        for index, sample in enumerate(samples):
+            if key not in sample:
+                has_error = True
+                if self._raise_error_key_missing:
+                    raise Exception(f"Error: key {key} does not exist in sample {index}: {sample}")
+                else:
+                    value = None
+            else:
+                value = sample[key]
+            collected_values.append(value)
+        return collected_values, has_error
