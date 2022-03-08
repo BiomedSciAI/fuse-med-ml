1538ac594e5e2d291b156ec5aa7cee87f573a7e4
moshiko <moshiko.raboh@ibm.com>
2022-03-08 15:32:35 +0200

    adding ci-cd

---
+++ fuse/utils/remote_execution/shell_handler.py
@@ -0,0 +1,65 @@
+import paramiko
+import re
+
+class ShellHandler:
+
+    def __init__(self, host, user, psw):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(host, username=user, password=psw, port=22)
+
+        channel = self.ssh.invoke_shell()
+        self.stdin = channel.makefile('wb')
+        self.stdout = channel.makefile('r')
+
+    def __del__(self):
+        self.ssh.close()
+
+    def execute(self, cmd):
+        """
+
+        @param cmd: the command to be executed on the remote computer
+        :examples:  execute('ls')
+                    execute('finger')
+                    execute('cd folder_name')
+        """
+        cmd = cmd.strip('\n')
+        self.stdin.write(cmd + '\n')
+        finish = 'end of stdOUT buffer. finished with exit status'
+        echo_cmd = 'echo {} $?'.format(finish)
+        self.stdin.write(echo_cmd + '\n')
+        shin = self.stdin
+        self.stdin.flush()
+
+        shout = []
+        sherr = []
+        exit_status = 0
+        for line in self.stdout:
+            if str(line).startswith(cmd) or str(line).startswith(echo_cmd):
+                # up for now filled with shell junk from stdin
+                shout = []
+            elif str(line).startswith(finish):
+                # our finish command ends with the exit status
+                exit_status = int(str(line).rsplit(maxsplit=1)[1])
+                if exit_status:
+                    # stderr is combined with stdout.
+                    # thus, swap sherr with shout in a case of failure.
+                    sherr = shout
+                    shout = []
+                break
+            else:
+                # get rid of 'coloring and formatting' special characters
+                shout.append(re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).
+                             replace('\b', '').replace('\r', ''))
+
+        # first and last lines of shout/sherr contain a prompt
+        if shout and echo_cmd in shout[-1]:
+            shout.pop()
+        if shout and cmd in shout[0]:
+            shout.pop(0)
+        if sherr and echo_cmd in sherr[-1]:
+            sherr.pop()
+        if sherr and cmd in sherr[0]:
+            sherr.pop(0)
+
+        return shin, shout, sherr
---
+++ run_all_unit_tests.py
@@ -0,0 +1,58 @@
+'''
+This script searches recursively for unit tests and generates the tests results in xml format in the way that Jenkins expects.
+In the case that it's a Jenkins job, it should delete any created cache (not implemented yet)
+'''
+
+import unittest
+import os
+print(os.path.dirname(os.path.realpath(__file__)))
+import termcolor
+def mehikon(a,b):
+    print(a)
+termcolor.cprint = mehikon  #since junit/jenkins doesn't like text color ...
+import xmlrunner
+
+if __name__ == '__main__':
+    os.environ['DISPLAY'] = '' #disable display in unit tests
+
+    is_jenkins_job = 'WORKSPACE' in os.environ and len(os.environ['WORKSPACE'])>2
+
+    search_base = os.path.dirname(os.path.realpath(__file__))
+    output = f"{search_base}/test-reports/"
+    print('will generate unit tests output xml at :',output)
+
+    # with open(f'{search_base}/packages.txt','r') as f:
+    #     sub_sections = [x.split('#')[-1].strip()+'/fuse/' for x in f.readlines() if len(x)>4]
+    sub_sections = ["."]
+    print('found sub_sections = ', sub_sections)
+
+    suite = None
+    for curr_subsection in sub_sections:
+        curr_subsuite = unittest.TestLoader().discover(
+            f'{search_base}/{curr_subsection}', 'test*.py'
+        )
+        if suite is None:
+            suite = curr_subsuite
+        else:
+            suite.addTest(curr_subsuite)
+            
+
+    test_results = xmlrunner.XMLTestRunner(output=output).run(
+        suite,
+    )
+
+    
+#TODO: think about caches and how we deal with them in unit tests (where they are found and who is responsible to delete them)
+#    if is_jenkins_job:
+#        print('jenkins job is not supported yet, it requires a caching location etc.')
+#        raise NotImplementedError
+
+#        #TODO: delete the created cache
+#        cache_path = caching.get_current_read_locations(caching_kind='user_local')
+#        assert 1 == len(cache_path)
+#        cache_path = cache_path[0]
+#        assert 'ms_img_analytics' in cache_path
+#        #delete the cache that we've created for this job
+#        print(f'deleting eir cache {cache_path} ...')
+#        os.system(f'rm -rf {cache_path}')
+#        print('done deleting eir cache.')
