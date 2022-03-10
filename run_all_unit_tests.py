d70daf6aa7d913fdff37637f050fd5b189be9fe2
moshiko <moshiko.raboh@ibm.com>
2022-03-10 09:30:39 +0200

    limit test runner output

--- run_all_unit_tests.py
+++ run_all_unit_tests.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
             suite.addTest(curr_subsuite)
             
 
-    test_results = xmlrunner.XMLTestRunner(output=output, verbosity=2, stream=sys.stdout).run(
+    test_results = xmlrunner.XMLTestRunner(output=output, verbosity=2).run(
         suite, 
     )
 
