2704b68e41bf6fd7707f6c37582f570b1dd93877
#Sagi Polaczek#Sagi.Polaczek@ibm.com <sagip@lsf-gpu3.haifa.ibm.com>
2022-03-21 22:46:07 +0200

    Delete unnecessary code and open a utils dir

---
+++ fuse_examples/tutorials/hello_world/hello_world_utils.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn.functional as F
+
+def perform_softmax(output):
+    if isinstance(output, torch.Tensor):  # validation
+        logits = output
+    else:  # train
+        logits = output.logits
+    cls_preds = F.softmax(logits, dim=1)
+    return logits, cls_preds
