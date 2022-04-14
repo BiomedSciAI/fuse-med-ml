587229ad144252eb9b69a99bb80d1bc80838244e
moshiko <moshiko.raboh@ibm.com>
2022-04-14 17:11:45 +0300

    test 3

--- fuse_examples/tests/test_notebook_hello_world.py
+++ fuse_examples/tests/test_notebook_hello_world.py
@@ -6,10 +6,6 @@ import fuse.utils.gpu as FuseUtilsGPU
 class NotebookHelloWorldTestCase(unittest.TestCase):
     
     def test_notebook(self):
-        num_gpus_allocated = FuseUtilsGPU.choose_and_enable_multiple_gpus(1, use_cpu_if_fail=True)
-        if num_gpus_allocated == 0:
-            assert False, "fail to allocate gpu"
-
         # Execute the whole notebook and save it as an object
         with testbook('fuse_examples/tutorials/hello_world/hello_world.ipynb', execute=range(3), timeout=600) as tb:
             print(tb.cell_output_text(2))
