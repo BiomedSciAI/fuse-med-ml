607b9b751c90359c9fa6f286dca44d608632bf88
sagi <sagi.polaczek@ibm.com>
2022-04-14 13:35:38 +0300

    Another try to run the notebook on a gpu

--- fuse_examples/tests/test_notebook_hello_world.py
+++ fuse_examples/tests/test_notebook_hello_world.py
@@ -6,11 +6,6 @@ import fuse.utils.gpu as FuseUtilsGPU
 class NotebookHelloWorldTestCase(unittest.TestCase):
     
     def test_notebook(self):
-        NUM_GPUS = 1
-        force_gpus = [0]
-        FuseUtilsGPU.choose_and_enable_multiple_gpus(NUM_GPUS, force_gpus=force_gpus)
-
-
         # Execute the whole notebook and save it as an object
         with testbook('fuse_examples/tutorials/hello_world/hello_world.ipynb', execute=True, timeout=600) as tb:
 
