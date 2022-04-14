1c11afee89abdca624692fe331e5ad04c9f7b28e
sagi <sagi.polaczek@ibm.com>
2022-04-14 12:37:04 +0300

    forced gpu 0 - a test run

--- fuse_examples/tests/test_notebook_hello_world.py
+++ fuse_examples/tests/test_notebook_hello_world.py
@@ -1,10 +1,17 @@
 import os
 import unittest
 from testbook import testbook
+from fuse.templates.walkthrough_template import NUM_GPUS
+import fuse.utils.gpu as FuseUtilsGPU
 
 class NotebookHelloWorldTestCase(unittest.TestCase):
     
-    def test_example_2(self):
+    def test_notebook(self):
+        NUM_GPUS = 1
+        force_gpus = [0]
+        FuseUtilsGPU.choose_and_enable_multiple_gpus(NUM_GPUS, force_gpus=force_gpus)
+
+
         # Execute the whole notebook and save it as an object
         with testbook('fuse_examples/tutorials/hello_world/hello_world.ipynb', execute=True, timeout=600) as tb:
 
