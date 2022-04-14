533f2526e6f7de31dfead3b0d09863c551e5e42c
Alex Golts <alex.golts@ibm.com>
2022-04-14 14:44:08 +0300

    pass targets to ensemble metric

--- fuse/eval/metrics/classification/metrics_ensembling_common.py
+++ fuse/eval/metrics/classification/metrics_ensembling_common.py
@@ -13,22 +13,25 @@ from .metrics_classification_common import MetricMultiClassDefault
 from functools import partial
 
 class  MetricEnsemble(MetricDefault):
-    def __init__(self, preds: str, output_file: Optional[str]=None, **kwargs):
+    def __init__(self, preds: str, target: Optional[str]=None, output_file: Optional[str]=None, **kwargs):
         """
         :param preds: key name for the multiple model prediction scores
         """
         ensemble = partial(self._ensemble, output_file=output_file)
-        super().__init__(metric_func=ensemble, preds=preds, extract_ids=True, **kwargs)
+        super().__init__(metric_func=ensemble, target=target, preds=preds, extract_ids=True, **kwargs)
 
-    def _ensemble(self, preds: Sequence[np.ndarray], ids: Sequence[Hashable], output_file: Optional[str]=None):
+    def _ensemble(self, preds: Sequence[np.ndarray], ids: Sequence[Hashable], target: Optional[Sequence[np.ndarray]]=None, output_file: Optional[str]=None):
 
         ensemble_preds = Ensembling.ensemble(preds=preds)
         # make sure to return the per-sample metric result for the relevant sample ids:
         per_sample_data = PerSampleData(data=ensemble_preds, ids=ids)
-
+        if target is not None:
+            per_sample_target = PerSampleData(data=target, ids=ids)
         if output_file is not None:
             df = pd.DataFrame()
             df['id'] = ids
             df['preds'] = per_sample_data(ids)
+            if target is not None:
+                df['target'] = per_sample_target(ids)
             df.to_pickle(output_file, compression='gzip')
-        return {'preds_ensembled': per_sample_data}
+        return {'preds_ensembled': per_sample_data, 'target': target}
