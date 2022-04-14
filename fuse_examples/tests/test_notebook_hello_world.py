e441ba82a657b6492531a7a7699cd8e08a22178e
moshiko <moshiko.raboh@ibm.com>
2022-04-14 16:49:44 +0300

    cancel git clone

--- fuse_examples/tests/test_notebook_hello_world.py
+++ fuse_examples/tests/test_notebook_hello_world.py
@@ -8,7 +8,7 @@ class NotebookHelloWorldTestCase(unittest.TestCase):
     def test_notebook(self):
         # Execute the whole notebook and save it as an object
         with testbook('fuse_examples/tutorials/hello_world/hello_world.ipynb', execute=True, timeout=600) as tb:
-
+            print(tb.cell_output_text(2))
             test_result_acc = tb.ref("test_result_acc")
             assert(test_result_acc > 0.95)
 
