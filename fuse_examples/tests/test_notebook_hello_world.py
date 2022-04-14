d5b00c6270424ecce60f4b2fc47ab52154628aa2
sagi <sagi.polaczek@ibm.com>
2022-04-14 18:02:39 +0300

    Sync with remote

--- fuse_examples/tests/test_notebook_hello_world.py
+++ fuse_examples/tests/test_notebook_hello_world.py
@@ -6,10 +6,13 @@ import fuse.utils.gpu as FuseUtilsGPU
 class NotebookHelloWorldTestCase(unittest.TestCase):
     
     def test_notebook(self):
+        notebook_path = "fuse_examples/tutorials/hello_world/hello_world.ipynb"
         # Execute the whole notebook and save it as an object
-        with testbook('fuse_examples/tutorials/hello_world/hello_world.ipynb', execute=True, timeout=600) as tb:
-
+        with testbook(notebook_path, execute=True, timeout=600) as tb:
+            # TODO!!!!
+            # Fix that it won't install each tim
             test_result_acc = tb.ref("test_result_acc")
+            tb.cell_output_text(1)
             assert(test_result_acc > 0.95)
 
 
