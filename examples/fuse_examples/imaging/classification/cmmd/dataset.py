ecad22d284432a097811ddf89601decbe6b461b5
itaiguez <itai.guez@ibm.com>
2022-05-02 17:55:14 +0300

    changing to fuse2

--- examples/fuse_examples/imaging/classification/cmmd/dataset.py
+++ examples/fuse_examples/imaging/classification/cmmd/dataset.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Callable, Optional
+from typing import Callable, Optional , Sequence
 import logging
 import pandas as pd
 import pydicom
@@ -7,20 +7,57 @@ import os, glob
 from pathlib import Path
 
 
-from fuse.data.visualizer.visualizer_default import VisualizerDefault
-from fuse.data.augmentor.augmentor_default import AugmentorDefault
-from fuse.data.augmentor.augmentor_toolbox import aug_op_color, aug_op_gaussian, aug_op_affine
-from fuse.data.dataset.dataset_default import DatasetDefault
-
+from fuse.data.pipelines.pipeline_default import PipelineDefault
+from fuse.data.datasets.dataset_default import DatasetDefault
+from fuse.data.datasets.caching.samples_cacher import SamplesCacher
+from fuseimg.data.ops.image_loader import OpLoadImage , OpLoadDicom
+from fuseimg.data.ops.color import OpClip, OpToRange
+from fuse.data import PipelineDefault, OpSampleAndRepeat, OpToTensor, OpRepeat
+from fuseimg.data.ops.aug.color import OpAugColor
+from fuseimg.data.ops.aug.geometry import OpAugAffine2D , OpAugCropAndResize2D
+from fuse.data.ops.ops_aug_common import OpSample
+from fuse.data.ops.op_base import OpBase
+from fuse.utils.ndict import NDict
+import torch
+from fuse.utils.rand.param_sampler import RandBool, RandInt, Uniform
 from fuse.utils.rand.param_sampler import Uniform, RandInt, RandBool
 
-from fuse_examples.imaging.classification.cmmd.input_processor import MGInputProcessor
-from fuse_examples.imaging.classification.cmmd.ground_truth_processor import MGGroundTruthProcessor
-from fuse.data.data_source.data_source_folds import DataSourceFolds
-
+# from fuse_examples.imaging.classification.cmmd.input_processor import MGInputProcessor
+# from fuse_examples.imaging.classification.cmmd.ground_truth_processor import MGGroundTruthProcessor
+from fuse.data.utils.split import SplitDataset
+from tempfile import mkdtemp
 from typing import Tuple
+from fuse.data.utils.sample import get_sample_id
 
+class OpCmmdDecode(OpBase):
+    '''
+    decodes sample id into image and segmentation filename
+    '''
+    """
+    Op that extract data from pytorch dataset that returning sequence of values and adds those values to sample_dict
+    """
 
+    def __init__(self, df : pd.DataFrame, sample_keys: Sequence[str]):
+        """
+        :param dataset: the pytorch dataset to convert. The dataset[i] expected to return sequence of values or a single value
+        :param sample_keys: sequence keys - naming each value returned by dataset[i]
+        """
+        # store input arguments
+        super().__init__()
+        self._sample_keys = sample_keys
+        self._df = df
+    def __call__(self, sample_dict: NDict,  op_id: Optional[str]) -> NDict:
+        '''
+        
+        '''
+        sample_keys=('data.image', 'data.label')
+        name, dataset_index = get_sample_id(sample_keys)
+        sid = self._df.iloc[dataset_index]['file']
+        
+        img_filename_key = 'data.input.img_path'
+        sample_dict[img_filename_key] = sid
+        return sample_dict
+    
 def CMMD_2021_dataset(data_dir: str, data_misc_dir: str ,cache_dir: str = 'cache', reset_cache: bool = False,
                       post_cache_processing_func: Optional[Callable] = None) -> Tuple[DatasetDefault, DatasetDefault]:
     """
@@ -32,118 +69,158 @@ def CMMD_2021_dataset(data_dir: str, data_misc_dir: str ,cache_dir: str = 'cache
     :param post_cache_processing_func:  Optional, function run post cache processing
     :return: training, validation and test DatasetDefault objects
     """
-    augmentation_pipeline = [
-        [
-            ('data.input.image',),
-            aug_op_affine,
-            {'rotate': Uniform(-30.0, 30.0), 'translate': (RandInt(-10, 10), RandInt(-10, 10)),
-             'flip': (RandBool(0.3), RandBool(0.3)), 'scale': Uniform(0.9, 1.1)},
-            {'apply': RandBool(0.5)}
-        ],
-        [
-            ('data.input.image',),
-            aug_op_color,
-            {'add': Uniform(-0.06, 0.06), 'mul': Uniform(0.95, 1.05), 'gamma': Uniform(0.9, 1.1),
-             'contrast': Uniform(0.85, 1.15)},
-            {'apply': RandBool(0.5)}
-        ],
-        [
-            ('data.input.image',),
-            aug_op_gaussian,
-            {'std': 0.03},
-            {'apply': RandBool(0.5)}
-        ],
-    ]
+    # augmentation_pipeline = [
+    #     [
+    #         ('data.input.image',),
+    #         aug_op_affine,
+    #         {'rotate': Uniform(-30.0, 30.0), 'translate': (RandInt(-10, 10), RandInt(-10, 10)),
+    #          'flip': (RandBool(0.3), RandBool(0.3)), 'scale': Uniform(0.9, 1.1)},
+    #         {'apply': RandBool(0.5)}
+    #     ],
+    #     [
+    #         ('data.input.image',),
+    #         aug_op_color,
+    #         {'add': Uniform(-0.06, 0.06), 'mul': Uniform(0.95, 1.05), 'gamma': Uniform(0.9, 1.1),
+    #          'contrast': Uniform(0.85, 1.15)},
+    #         {'apply': RandBool(0.5)}
+    #     ],
+    #     [
+    #         ('data.input.image',),
+    #         aug_op_gaussian,
+    #         {'std': 0.03},
+    #         {'apply': RandBool(0.5)}
+    #     ],
+    # ]
+
 
     lgr = logging.getLogger('Fuse')
     target = 'classification'
     input_source_gt = merge_clinical_data_with_dicom_tags(data_dir, data_misc_dir, target)
+    input_df = pd.read_csv(input_source_gt)
+    folds_df = SplitDataset.balanced_division(df = input_df ,
+                                                no_mixture_id = 'ID1',
+                                                key_columns = [target] ,
+                                                nfolds = 5 ,
+                                                print_flag=True )
+    sample_ids=[id for id in range(len(folds_df))]
 
-    partition_file_path = os.path.join(data_misc_dir, 'data_fold_new.csv')
-    train_data_source = DataSourceFolds(input_source=input_source_gt,
-                                            input_df=None,
-                                            phase='train',
-                                            no_mixture_id='ID1',
-                                            balance_keys=[target],
-                                            reset_partition_file=True,
-                                            folds=[0,1,2],
-                                            num_folds=5,
-                                            partition_file_name=partition_file_path)
+    static_pipeline = PipelineDefault("static", [
+        (OpCmmdDecode(folds_df,sample_ids), dict()), # will save image and seg path to "data.input.img_path", "data.gt.seg_path" 
+        (OpLoadDicom(data_dir), dict(key_in="data.input.img_path", key_out="data.input.img", format="nib")),
+        (OpToRange(), dict(key="data.input.img", from_range=(0, 255), to_range=(0, 1))),
+        (OpAugCropAndResize2D(), dict(key="data.input.img", from_range=(0, 255), to_range=(0, 1))),
+        ])
+
+    dynamic_pipeline = PipelineDefault("dynamic", [
+        (OpToTensor(), dict(dtype=torch.float32)),
+        (OpSample(OpAugAffine2D()), dict(
+                        rotate=Uniform(-30.0,30.0),        
+                        scale=Uniform(0.9, 1.1),
+                        flip=(RandBool(0.3), RandBool(0.5)),
+                        translate=(RandInt(-10, 10), RandInt(-10, 10))
+                    )),
+        (OpSample(OpAugColor()), dict(
+                    key="data.input.img",
+                    gamma=Uniform(0.9, 1.1), 
+                    contrast=Uniform(0.85, 1.15),
+                    mul =  Uniform(0.95, 1.05),
+                    add=Uniform(-0.06, 0.06)
+                )),
+    ])
+                                       
+    cache_dir = mkdtemp(prefix="cmmd")
+    cacher = SamplesCacher(f'cmmd_cache_ver', 
+        static_pipeline,
+        cache_dirs=[cache_dir], restart_cache=True)   
+    
+    my_dataset = DatasetDefault(sample_ids=sample_ids,
+        static_pipeline=static_pipeline,
+        dynamic_pipeline=dynamic_pipeline,
+        cacher=cacher,            
+    )
+
+
+    my_dataset.create()
+    
+    # partition_file_path = os.path.join(data_misc_dir, 'data_fold_new.csv')
+    
+    # train_data_source = DataSourceFolds(input_source=input_source_gt,
+    #                                         input_df=None,
+    #                                         phase='train',
+    #                                         no_mixture_id='ID1',
+    #                                         balance_keys=[target],
+    #                                         reset_partition_file=True,
+    #                                         folds=[0,1,2],
+    #                                         num_folds=5,
+    #                                         partition_file_name=partition_file_path)
+
     
 
-    # Create data processors:
-    input_processors = {
-        'image': MGInputProcessor(input_data=data_dir)
-    }
-    gt_processors = {
-        'classification': MGGroundTruthProcessor(input_data=input_source_gt)
-    }
+    # # Create data processors:
+    # input_processors = {
+    #     'image': MGInputProcessor(input_data=data_dir)
+    # }
+    # gt_processors = {
+    #     'classification': MGGroundTruthProcessor(input_data=input_source_gt)
+    # }
 
-    # Create data augmentation (optional)
-    augmentor = AugmentorDefault(
-        augmentation_pipeline=augmentation_pipeline)
 
-    # Create visualizer (optional)
-    visualiser = VisualizerDefault(image_name='data.input.image', label_name='data.gt.classification')
 
-    # Create train dataset
-    train_dataset = DatasetDefault(cache_dest=cache_dir,
-                                       data_source=train_data_source,
-                                       input_processors=input_processors,
-                                       gt_processors=gt_processors,
-                                       post_processing_func=post_cache_processing_func,
-                                       augmentor=augmentor,
-                                       visualizer=visualiser)
+    # # Create train dataset
+    # train_dataset = DatasetDefault(cache_dest=cache_dir,
+    #                                    data_source=train_data_source,
+    #                                    input_processors=input_processors,
+    #                                    gt_processors=gt_processors,
+    #                                    post_processing_func=post_cache_processing_func,
+    #                                    augmentor=augmentor)
 
-    lgr.info(f'- Load and cache data:')
-    train_dataset.create(reset_cache=reset_cache)
-    lgr.info(f'- Load and cache data: Done')
+    # lgr.info(f'- Load and cache data:')
+    # train_dataset.create(reset_cache=reset_cache)
+    # lgr.info(f'- Load and cache data: Done')
 
-    # Create validation data source
-    validation_data_source = DataSourceFolds(input_source=input_source_gt,
-                                            input_df=None,
-                                            phase='validation',
-                                            no_mixture_id='ID1',
-                                            balance_keys=[target],
-                                            reset_partition_file=False,
-                                            folds=[3],
-                                            num_folds=5,
-                                            partition_file_name=partition_file_path)
+    # # Create validation data source
+    # validation_data_source = DataSourceFolds(input_source=input_source_gt,
+    #                                         input_df=None,
+    #                                         phase='validation',
+    #                                         no_mixture_id='ID1',
+    #                                         balance_keys=[target],
+    #                                         reset_partition_file=False,
+    #                                         folds=[3],
+    #                                         num_folds=5,
+    #                                         partition_file_name=partition_file_path)
 
-    ## Create dataset
-    validation_dataset = DatasetDefault(cache_dest=cache_dir,
-                                            data_source=validation_data_source,
-                                            input_processors=input_processors,
-                                            gt_processors=gt_processors,
-                                            post_processing_func=post_cache_processing_func,
-                                            augmentor=None,
-                                            visualizer=visualiser)
-    validation_dataset.create( pool_type='thread')  # use ThreadPool to create this dataset, to avoid cv2 problems in multithreading
+    # ## Create dataset
+    # validation_dataset = DatasetDefault(cache_dest=cache_dir,
+    #                                         data_source=validation_data_source,
+    #                                         input_processors=input_processors,
+    #                                         gt_processors=gt_processors,
+    #                                         post_processing_func=post_cache_processing_func,
+    #                                         augmentor=None)
+    # validation_dataset.create( pool_type='thread')  # use ThreadPool to create this dataset, to avoid cv2 problems in multithreading
 
-    test_data_source =  DataSourceFolds(input_source=input_source_gt,
-                                            input_df=None,
-                                            phase='test',
-                                            no_mixture_id='ID1',
-                                            balance_keys=[target],
-                                            reset_partition_file=False,
-                                            folds=[4],
-                                            num_folds=5,
-                                            partition_file_name=partition_file_path)
-    test_dataset = DatasetDefault(cache_dest=cache_dir,
-                                            data_source=test_data_source,
-                                            input_processors=input_processors,
-                                            gt_processors=gt_processors,
-                                            post_processing_func=post_cache_processing_func,
-                                            augmentor=None,
-                                            visualizer=visualiser)
+    # test_data_source =  DataSourceFolds(input_source=input_source_gt,
+    #                                         input_df=None,
+    #                                         phase='test',
+    #                                         no_mixture_id='ID1',
+    #                                         balance_keys=[target],
+    #                                         reset_partition_file=False,
+    #                                         folds=[4],
+    #                                         num_folds=5,
+    #                                         partition_file_name=partition_file_path)
+    # test_dataset = DatasetDefault(cache_dest=cache_dir,
+    #                                         data_source=test_data_source,
+    #                                         input_processors=input_processors,
+    #                                         gt_processors=gt_processors,
+    #                                         post_processing_func=post_cache_processing_func,
+    #                                         augmentor=None)
     
-    test_dataset.create( pool_type='thread')  # use ThreadPool to create this dataset, to avoid cv2 problems in multithreading
+    # test_dataset.create( pool_type='thread')  # use ThreadPool to create this dataset, to avoid cv2 problems in multithreading
 
     lgr.info(f'- Load and cache data:')
 
     lgr.info(f'- Load and cache data: Done')
 
-    return train_dataset, validation_dataset, test_dataset
+    return my_dataset, my_dataset, my_dataset
 
 
 def merge_clinical_data_with_dicom_tags(data_dir: str, data_misc_dir:str, target: str) -> str:
--- fuseimg/data/ops/image_loader.py
+++ fuseimg/data/ops/image_loader.py
@@ -2,10 +2,12 @@ import os
 from fuse.data.ops.op_base import OpBase
 from typing import Optional
 import numpy as np
+import pandas as pd
 from fuse.data.ops.ops_common import OpApplyTypes
 import nibabel as nib
 from fuse.utils.ndict import NDict
 import skimage.io as io
+import pydicom
 
 class OpLoadImage(OpBase):
     '''
@@ -26,6 +28,38 @@ class OpLoadImage(OpBase):
             (format in ["nii", "nib"]):  
             img = nib.load(img_filename)
             img_np = img.get_fdata()
+        else:
+            raise Exception(f"OpLoadImage: case format {format} and {img_filename_suffix} is not supported")
+        
+        sample_dict[key_out] = img_np
+
+        return sample_dict
+
+    def reverse(self, sample_dict: NDict, key_to_reverse: str, key_to_follow: str, op_id: Optional[str]) -> dict:        
+        return sample_dict 
+    
+    
+class OpLoadDicom(OpBase):
+    '''
+    Loads a medical image, currently only nii is supported
+    '''
+    def __init__(self, dir_path: str , **kwargs):
+        super().__init__(**kwargs)
+        self._dir_path = dir_path
+
+    def __call__(self, sample_dict: NDict, op_id: Optional[str], key_in:str, key_out: str, format:str="infer"):
+        '''
+        :param key_in: the key name in sample_dict that holds the filename
+        :param key_out: 
+        '''
+        img_filename = os.path.join(self._dir_path, sample_dict[key_in])
+        img_filename_suffix = img_filename.split(".")[-1]
+        if (format == "infer" and img_filename_suffix in ["dcm"]) or \
+            (format in ["nii", "nib"]):  
+            dcm = pydicom.dcmread(img_filename)
+            inner_image = dcm.pixel_array
+            # convert to numpy
+            img_np = np.asarray(inner_image)
         else:
             raise Exception(f"OpLoadImage: case format {format} and {img_filename_suffix} is not supported")
         
