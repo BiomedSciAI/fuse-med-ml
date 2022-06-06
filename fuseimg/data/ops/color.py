f2f783f59acd00e2c940678486ea1fd3a18506f8
itaiguez <itai.guez@ibm.com>
2022-06-06 14:44:13 +0300

    remove comment

--- fuseimg/data/ops/color.py
+++ fuseimg/data/ops/color.py
@@ -56,7 +56,6 @@ class OpNormalizeAgainstSelf(OpBase):
     def __call__(self, sample_dict: NDict, key: str):
         img = sample_dict[key]
         img -= img.min()
-        #np.divide(img, img.max(), out=img, casting="unsafe")
         img = skimage.img_as_float(img)
         sample_dict[key] = img
         return sample_dict
