35dbeee47f0de3462d0cf96b957397de402ba4c8
Moshiko Raboh <86309179+mosheraboh@users.noreply.github.com>
2022-06-12 14:53:44 +0300

    walkthrough template to fuse2 style (#99)

--- fuse/dl/models/model_wrapper.py
+++ fuse/dl/models/model_wrapper.py
@@ -17,12 +17,10 @@ Created on June 30, 2021
 
 """
 
-from typing import Sequence, Dict, Tuple, Callable
+from typing import Sequence, Dict, Callable
 
 import torch
 
-from fuse.dl.models.backbones.backbone_inception_resnet_v2 import BackboneInceptionResnetV2
-from fuse.dl.models.heads.head_global_pooling_classifier import HeadGlobalPoolingClassifier
 from fuse.utils.ndict import NDict
 
 
