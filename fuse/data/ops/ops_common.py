ae47099558c1a3d99fd10f01a48ac3ce62f1eea7
Sagi Polaczek <sagi.polaczek@gmail.com>
2022-07-10 17:08:36 +0300

    fixed import issue

--- fuse/data/ops/ops_common.py
+++ fuse/data/ops/ops_common.py
@@ -4,7 +4,8 @@ from typing import Any, Callable, Dict, List, Optional, OrderedDict, Sequence, T
 from fuse.data.key_types import TypeDetectorBase
 import copy
 from enum import Enum
-from .op_base import OpBase, OpReversibleBase, Patterns, op_call, op_reverse  # DataType,
+from .op_base import OpBase, OpReversibleBase, op_call, op_reverse  # DataType,
+from fuse.data.patterns import Patterns
 from fuse.utils.ndict import NDict
 import numpy as np
 
