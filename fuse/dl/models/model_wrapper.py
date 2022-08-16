9e669dadb9f31b9a5f387a8bc13f3ec6931b7685
Iti Guez <guez@phi-gpu-node2.sl.cloud9.ibm.com>
2022-08-16 08:18:18 -0500

    new wrapper

--- fuse/dl/models/model_wrapper.py
+++ fuse/dl/models/model_wrapper.py
@@ -84,3 +84,23 @@ class ModelWrapSeqToDict(torch.nn.Module):
                 batch_dict[output_name] = model_output[i]
 
         return batch_dict
+
+class ModelWrapDictToSeq(torch.nn.Module):
+    """
+    Fuse model wrapper for wrapping torch modules and passing through Fuse
+    """
+
+    def __init__(self, fuse_model : torch.nn.Module, output_key : str):
+        super().__init__()
+        self.model = fuse_model
+        self.output_key = output_key
+    def forward(self, input : torch.tensor):
+        batch_dict = NDict()
+        #find input key
+        fuse_input = self.model.conv_inputs[0][0]
+        batch_dict[fuse_input] = input
+        #feed fuse model with dict as he excpect
+        ans_ndict = self.model(batch_dict)
+        #extract model output from dict
+        output =ans_ndict['output'][self.output_key]
+        return output
