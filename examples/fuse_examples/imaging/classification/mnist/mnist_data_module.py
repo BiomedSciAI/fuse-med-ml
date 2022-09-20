ed2c15869431761096aa7b06cc2c092879178b06
Sagi Polaczek <sagi.polaczek@gmail.com>
2022-09-20 12:05:15 +0300

    remove prints

--- examples/fuse_examples/imaging/classification/mnist/mnist_data_module.py
+++ examples/fuse_examples/imaging/classification/mnist/mnist_data_module.py
@@ -25,7 +25,6 @@ class MNISTDataModule(pl.LightningDataModule):
         """
         train_dataset = MNIST.dataset(self._cache_dir, train=True)
 
-        print("- DataModule, Create sampler:")
         batch_sampler = BatchSamplerDefault(
             dataset=train_dataset,
             balanced_class_name="data.label",
@@ -33,16 +32,14 @@ class MNISTDataModule(pl.LightningDataModule):
             batch_size=self._batch_size,
             verbose=True,
         )
-        print("- DataModule, Create sampler: Done")
 
-        print("- DataModule, Create Dataloader:")
         train_dl = DataLoader(
             dataset=train_dataset,
             batch_sampler=batch_sampler,
             collate_fn=CollateDefault(),
             num_workers=self._num_workers,
         )
-        print("- DataModule, Create Dataloader: Done")
+
         return train_dl
 
     def val_dataloader(self):
