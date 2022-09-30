e0ae74afe5aecc7b1eedac0b047d28c297f9a0ee
Michal Ozery-Flato <ozery@il.ibm.com>
2022-09-30 20:17:18 +0300

    support time_unit in c-index

--- fuse/eval/metrics/libs/survival.py
+++ fuse/eval/metrics/libs/survival.py
@@ -1,10 +1,10 @@
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 from lifelines.utils import concordance_index
 import numpy as np
-
+from copy import copy
 class MetricsSurvival:
     @staticmethod
-    def c_index(pred: np.ndarray, event_times: np.ndarray = None, event_observed : np.ndarray = None, event_class_index: int = -1) -> float:
+    def c_index(pred: np.ndarray, event_times: np.ndarray = None, event_observed : np.ndarray = None, event_class_index: int = -1, time_unit:int=1) -> float:
         """
         Compute c-index (concordance index) score using lifelines
         :param pred: prediction array per sample. Each element is a score (scalar)
@@ -17,5 +17,10 @@ class MetricsSurvival:
 
         if isinstance(pred[0], np.ndarray):
             pred = np.asarray(pred)[:,event_class_index]
+
+        if time_unit > 1:
+            event_times = copy(event_times)
+            for i in range(len(event_times)):
+                event_times[i]  = int(event_times[i] / time_unit)
            
         return concordance_index(event_times, pred, event_observed) 
