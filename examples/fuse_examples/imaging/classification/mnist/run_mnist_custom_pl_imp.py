1e3c0ce3f09eed49f51e989a49c0a341eaa5d5c6
Daniel Shats Daniel.Shats1@ibm.com <shatz@cccxl016.pok.ibm.com>
2022-10-26 03:26:44 -0400

    need to make logger log to same path as our default logger

--- examples/fuse_examples/imaging/classification/mnist/run_mnist_custom_pl_imp.py
+++ examples/fuse_examples/imaging/classification/mnist/run_mnist_custom_pl_imp.py
@@ -31,6 +31,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
 
 from fuse.eval.evaluator import EvaluatorDefault
 from fuse.eval.metrics.classification.metrics_thresholding_common import MetricApplyThresholds
@@ -243,9 +244,11 @@ def perform_softmax(logits: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
 #################################
 def run_train(paths: dict, train_params: dict):
     # ==============================================================================
-    # Logger
+    # Logger(s)
     # ==============================================================================
     fuse_logger_start(output_path=paths["model_dir"], console_verbose_level=logging.INFO)
+    import pdb; pdb.set_trace()
+    lightning_csv_logger = CSVLogger("lightning_csv_logs", name="")
     print("Fuse Train")
 
     # ==============================================================================
@@ -305,6 +308,7 @@ def run_train(paths: dict, train_params: dict):
         strategy=train_params["trainer.strategy"],
         devices=train_params["trainer.num_devices"],
         auto_select_gpus=True,
+        # logger=lightning_csv_logger,
     )
 
     # train
