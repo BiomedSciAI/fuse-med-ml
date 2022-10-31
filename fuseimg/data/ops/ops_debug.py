bc807cf115f532b02738e3fd647eff329cfc5c05
Daniel Shats Daniel.Shats1@ibm.com <shatz@cccxc407.pok.ibm.com>
2022-10-31 11:04:13 -0400

    removed unecessary print statemet

--- fuseimg/data/ops/ops_debug.py
+++ fuseimg/data/ops/ops_debug.py
@@ -298,7 +298,6 @@ class OpVis3DPlotly(OpDebugBase):
         """
         # uncomment the below line for example from their website (brain MRI)
         # vol = io.imread("<https://s3.amazonaws.com/assets.datacamp.com/blog_assets/attention-mri.tif>")
-        print(f"--- DEBUG volu shape: {vol.shape}")
 
         nb_frames, w, h = vol.shape
         cmin = vol.min()
