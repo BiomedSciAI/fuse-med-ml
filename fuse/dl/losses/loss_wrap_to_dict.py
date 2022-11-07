60affd2a0b8d2ee94c0b47ba267da1502a3f100e
Avihu Dekel <avihu112@gmail.com>
2022-11-07 17:45:22 +0200

    Update loss_wrap_to_dict.py
    add a usage example

--- fuse/dl/losses/loss_wrap_to_dict.py
+++ fuse/dl/losses/loss_wrap_to_dict.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import torch
+from torch import nn
 from fuse.utils.ndict import NDict
 
 
@@ -24,3 +25,27 @@ class LossWrapToDict(torch.nn.Module):
         # run loss function
         loss = self._loss_module(**loss_kwargs)
         return loss
+
+
+def usage_example():
+    batch_size = 100
+    num_classes = 10
+    # creating an example of a batch_dict
+    logits = torch.randn(size=(batch_size, num_classes))
+    gt = torch.randint(high=num_classes, size=(batch_size,))
+    batch_dict = NDict({"data.gt": gt, "model.logits": logits})
+
+    # creating the loss
+    loss = nn.CrossEntropyLoss()
+    # the CrossEntropyLoss forward function has two arguments: input, target
+    # this maps every forward argument to the relevant key in the batch_dict
+    loss_arg_to_batch_key = {"input": "model.logits", "target": "data.gt"}
+    # wrapping the loss
+    wrapped_loss = LossWrapToDict(loss_module=loss, loss_arg_to_batch_key=loss_arg_to_batch_key)
+    # running the wrapped loss with the batch_dict
+    loss_result = wrapped_loss(batch_dict)
+    print(f"the output loss is: {loss_result}")
+
+
+if __name__ == "__main__":
+    usage_example()
