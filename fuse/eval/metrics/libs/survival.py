f241e262714c158da8a204b1bd44fb1ba5ef5b1f
Avihu-Dekel <avihu.dekel@ibm.com>
2022-11-10 12:51:30 +0200

    DeepHit implementation Expected C-index General loss wrapper

--- fuse/eval/metrics/libs/survival.py
+++ fuse/eval/metrics/libs/survival.py
@@ -1,4 +1,3 @@
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
 from lifelines.utils import concordance_index
 import numpy as np
 
@@ -7,8 +6,8 @@ class MetricsSurvival:
     @staticmethod
     def c_index(
         pred: np.ndarray,
-        event_times: np.ndarray = None,
-        event_observed: np.ndarray = None,
+        event_times: np.ndarray,
+        event_observed: np.ndarray,
         event_class_index: int = -1,
         time_unit: int = 1,
     ) -> float:
@@ -26,3 +25,49 @@ class MetricsSurvival:
         event_times = (np.array(event_times) / time_unit).astype(int)
 
         return concordance_index(event_times, -pred, event_observed)
+
+    @staticmethod
+    def expected_cindex(
+        pred: np.ndarray,
+        event_times: np.ndarray,
+        event_observed: np.ndarray,
+    ) -> float:
+        """
+        Compute c-index (concordance index) score using lifelines
+        :param pred: prediction array per sample. Each element is a score (scalar). Higher score - higher chance for the event
+        :param event_times: event/censor time array
+        :param event_observed: Optioal- a length-n iterable censoring flags, 1 if observed, 0 if not. Default None assumes all observed.
+        :return c-index (concordance index) score
+        """
+        # converting all to number
+        pred = np.array(pred)
+        event_times = np.array(event_times)
+        event_observed = np.array(event_observed)
+        # assuming the calculating the "expected time" until an event
+        num_bins = pred.shape[1]
+        # [1,...,0] - highest risk
+        # [0,...,0,1] - lowest risk
+        expected_event_time = (pred * np.arange(num_bins)).sum(axis=1)
+        return concordance_index(event_times, expected_event_time, event_observed)
+
+
+def run_example():
+    b = 10000
+    bins = 64
+    # creating probability distribution across bins
+    pred = np.random.random(size=(b, bins))
+    pred = pred / pred.sum(axis=1, keepdims=True)
+    # creating a "has event" binary variable per example
+    event_observed = np.random.random(b) > 0.5
+    # creating a list of followup times
+    event_times = np.random.randint(low=1, high=100, size=b)
+
+    expected_time_cindex = MetricsSurvival.expected_cindex(
+        pred=pred, event_times=event_times, event_observed=event_observed
+    )
+    # should be close to 0.5, considering it's all random
+    print(f"expected c-index is {expected_time_cindex}")
+
+
+if __name__ == "__main__":
+    run_example()
