034ece1693ef074992c3626250b711c144321bd3
Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxl014.pok.ibm.com>
2022-12-11 07:29:57 -0500

    support arbitrary (not sequenctial and even not ints) balanced class values

--- fuse/data/ops/ops_common.py
+++ fuse/data/ops/ops_common.py
@@ -509,3 +509,14 @@ class OpCond(OpBase):
             return self._op(sample_dict, **kwargs)
         else:
             return sample_dict
+
+class OpSet(OpBase):
+    """Add/override key-value pair into sample_dict"""
+    def __call__(self, sample_dict: NDict, key: str, value: Any) -> Union[None, dict, List[dict]]:
+        """
+        :param key: where to store the value
+        :param value: the value to store
+        """
+        sample_dict[key] = value
+        return sample_dict
+
