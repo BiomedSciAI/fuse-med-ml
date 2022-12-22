afd1d38503d0641f2d2c3ddaf2b7b1aab86ff1e4
Sagi Polaczek <sagi.polaczek@ibm.com>
2022-12-22 17:31:46 +0200

    fxied one line

--- fuse/dl/models/backbones/backbone_mlp.py
+++ fuse/dl/models/backbones/backbone_mlp.py
@@ -47,7 +47,6 @@ class BackboneMultilayerPerceptron(torch.nn.Module):
         self.mlp = nn.ModuleList(mlp_layers)
 
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        input_tensor = input_tensor.float()
 
         for layer in self.mlp:
             input_tensor = layer(input_tensor)
