1fa5c2fd915935204ba0e576cfdfa2420eca04ee
Ella Barkan <ella@il.ibm.com>
2023-01-19 16:20:21 +0200

    Ehr transformer example (#256)
    * Adding unitest for ehr_transformer
    
    * Adding unitest for ehr_transformer
    
    * Fixed to unitest
    
    * Fixed to unitest
    
    * Fixed to unitest
    
    * Fixed to unitest
    
    * Fixed merging messages
    
    * Added temp dir for the parallel  tests
    
    Co-authored-by: Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxl016.pok.ibm.com>
    Co-authored-by: Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxc409.pok.ibm.com>
    Co-authored-by: Moshiko Raboh <86309179+mosheraboh@users.noreply.github.com>

--- examples/fuse_examples/multimodality/ehr_transformer/main_train.py
+++ examples/fuse_examples/multimodality/ehr_transformer/main_train.py
@@ -1,4 +1,3 @@
-from dataset import PhysioNetCinC
 from typing import Any, Optional, Tuple
 import hydra
 from omegaconf import DictConfig
@@ -21,8 +20,8 @@ from fuse.eval.metrics.classification.metrics_classification_common import Metri
 from fuse.dl.losses import LossDefault
 from fuse.utils import NDict
 
-
 from examples.fuse_examples.multimodality.ehr_transformer.model import Embed, TransformerEncoder, Bert, BertConfig
+from examples.fuse_examples.multimodality.ehr_transformer.dataset import PhysioNetCinC
 
 
 def filter_gender_label_unknown_for_loss(batch_dict: NDict) -> NDict:
