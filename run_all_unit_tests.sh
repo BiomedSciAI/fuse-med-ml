12b5c096d9734d09f5d9f69b29549668aab610e1
Sagi Polaczek <sagi.polaczek@ibm.com>
2023-02-07 16:05:23 +0200

    make it create env, 2

--- run_all_unit_tests.sh
+++ run_all_unit_tests.sh
@@ -56,7 +56,7 @@ create_env() {
 
         nvidia-smi
 
-        if false ; then
+        if find_in_conda_env $ENV_NAME ; then
             echo "Environment exist: $env"
         else
             echo "Mode=$mode"
