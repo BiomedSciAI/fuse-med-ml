2800b50bd20fc9a10ec58affed4d03b4b2e67ebb
Itai Guez Itai.Guez@ibm.com <guez@cccxl012.pok.ibm.com>
2023-05-07 07:37:18 -0400

    black flake

--- fuse_examples/imaging/segmentation/PI-CAI/unet.py
+++ fuse_examples/imaging/segmentation/PI-CAI/unet.py
@@ -1,13 +1,11 @@
 from torch import nn
 from monai.networks.nets import UNet as UNetBase
 from fuse.utils.ndict import NDict
-import torch.nn.functional as F
 import torch
 
+
 class UNet(nn.Module):
-    def __init__(
-        self, input_name: str, seg_name: str, unet_kwargs: dict
-    ):
+    def __init__(self, input_name: str, seg_name: str, unet_kwargs: dict):
         super().__init__()
         self.input_name = input_name
         self.seg_name = seg_name
@@ -16,5 +14,5 @@ class UNet(nn.Module):
     def forward(self, batch_dict: NDict):
         x = batch_dict[self.input_name]
         seg_output = self.unet(x)
-        batch_dict[self.seg_name] = torch.unsqueeze(seg_output[:,0,:,:,:], 1)
+        batch_dict[self.seg_name] = torch.unsqueeze(seg_output[:, 0, :, :, :], 1)
         return batch_dict
