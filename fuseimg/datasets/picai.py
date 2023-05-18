693658aa6fa754d4453770d3dc039525705ec11e
Itai Guez Itai.Guez@ibm.com <guez@cccxc409.pok.ibm.com>
2023-05-18 11:41:58 -0400

    changed to whole gland segmentation

--- fuseimg/datasets/picai.py
+++ fuseimg/datasets/picai.py
@@ -82,6 +82,7 @@ class OpLoadPICAIImage(OpBase):
             return None
 
 
+# loads cancer segmentation - not in use for now
 class OpLoadPICAISegmentation(OpBase):
     """
     Loads a medical image
@@ -105,21 +106,60 @@ class OpLoadPICAISegmentation(OpBase):
         :param key_metadata_out : the key to hold metadata dictionary
         """
         # for seq in self._sequences:
-        img_filename = os.path.join(self._dir_path, sample_dict[key_in] + ".nii.gz")
-        if os.path.exists(img_filename):
-            my_img = nib.load(img_filename)
+        seg_filename = os.path.join(self._dir_path, sample_dict[key_in] + ".nii.gz")
+        if os.path.exists(seg_filename):
+            my_img = nib.load(seg_filename)
             nii_data = my_img.get_fdata()
-            sample_dict[key_out] = nii_data
         else:
             img_filename = os.path.join(
                 self._data_dir, sample_dict[key_in].split("_")[0], sample_dict[key_in] + "_t2w.mha"
             )
             image_data, image_header = load(img_filename)
             nii_data = np.zeros(image_data.shape)
-        if sample_dict[gt] == 1 and np.count_nonzero(nii_data) == 0:
-            print(sample_dict[key_in], "is cancer and has no segmentation")
+        unique_labels = np.unique(nii_data)
+        if sample_dict[gt] not in unique_labels:
+            print(sample_dict[key_in], "ISUP is", sample_dict[gt], "but unique labels are", unique_labels)
             return None
+        # TODO - generate different map according to different ISUP in cancer segmentation?
+        # else:
+        #     output = np.zeros([5]+image_data.shape)
+        #     for label in unique_labels:
+        #         if label != 0:
+        #             output[label-1] = nii_data[nii_data == label]
+        sample_dict[key_out] = nii_data
+        return sample_dict
 
+
+class OpLoadPICAISegmentationWholeGland(OpBase):
+    """
+    Loads a medical image
+    """
+
+    def __init__(self, data_dir: str, dir_path: str, **kwargs: dict):
+        super().__init__(**kwargs)
+        self._dir_path = dir_path
+        self._data_dir = data_dir
+
+    def __call__(
+        self,
+        sample_dict: NDict,
+        key_in: str,
+        key_out: str,
+        gt: str,
+    ) -> None:
+        """
+        :param key_in: the key name in sample_dict that holds the filename
+        :param key_out: the key name in sample_dict that holds the image
+        :param key_metadata_out : the key to hold metadata dictionary
+        """
+        # for seq in self._sequences:
+        seg_filename = os.path.join(self._dir_path, sample_dict[key_in] + ".nii.gz")
+        if os.path.exists(seg_filename):
+            my_img = nib.load(seg_filename)
+            nii_data = my_img.get_fdata()
+        else:
+            print(seg_filename, "missing segmentation file")
+            return None
         sample_dict[key_out] = nii_data
         return sample_dict
 
@@ -149,8 +189,20 @@ class PICAI:
                         key_column="index",
                         key_name="data.input.img_path",
                         #'psa','psad','prostate_volume','histopath_type','lesion_GS','lesion_ISUP','case_ISUP'
-                        columns_to_extract=["index", "patient_id", "study_id", "mri_date", "patient_age", "case_csPCa"],
-                        rename_columns=dict(patient_id="data.patientID", case_csPCa="data.gt.classification"),
+                        columns_to_extract=[
+                            "index",
+                            "patient_id",
+                            "study_id",
+                            "mri_date",
+                            "patient_age",
+                            "case_ISUP",
+                            "case_csPCa",
+                        ],
+                        rename_columns=dict(
+                            patient_id="data.patientID",
+                            case_csPCa="data.gt.classification",
+                            case_ISUP="data.gt.subtype",
+                        ),
                     ),
                     dict(),
                 ),
@@ -160,8 +212,8 @@ class PICAI:
                     dict(key_in="data.gt.classification", key_out="data.gt.classification_one_hot"),
                 ),
                 (
-                    OpLoadPICAISegmentation(data_dir, seg_dir),
-                    dict(key_in="data.input.img_path", key_out="data.gt.seg", gt="data.gt.classification"),
+                    OpLoadPICAISegmentationWholeGland(data_dir, seg_dir),
+                    dict(key_in="data.input.img_path", key_out="data.gt.seg", gt="data.gt.subtype"),
                 ),
                 (
                     OpRepeat(
@@ -188,7 +240,7 @@ class PICAI:
                 ),
                 # (OpNormalizeAgainstSelf(), dict(key="data.input.img_t2w")),
                 (OpRepeat((OpNormalizeAgainstSelf()), kwargs_per_step_to_add=repeat_images), dict()),
-                (OpRepeat((OpToNumpy()), kwargs_per_step_to_add=repeat_images_with_seg), dict(dtype=np.float32)),
+                (OpRepeat((OpToNumpy()), kwargs_per_step_to_add=repeat_images), dict(dtype=np.float32)),
                 # (OpResizeAndPad2D(), dict(key="data.input.img", resize_to=(2200, 1200), padding=(60, 60))),
             ],
         )
@@ -204,18 +256,20 @@ class PICAI:
         Get suggested dynamic pipeline. including pre-processing that might be modified and augmentation operations.
         :param train : True iff we request dataset for train purpouse
         """
+        repeat_images_with_seg = repeat_images + [dict(key="data.gt.seg")]
         ops = []
         ops += [
             (OpRepeat((OpToTensor()), kwargs_per_step_to_add=repeat_images), dict(dtype=torch.float32)),
-            (OpRepeat((OpLambda(partial(torch.unsqueeze, dim=0))), kwargs_per_step_to_add=repeat_images), {}),
+            (OpToTensor(), dict(key="data.gt.seg", dtype=torch.int32)),
+            (OpRepeat((OpLambda(partial(torch.unsqueeze, dim=0))), kwargs_per_step_to_add=repeat_images_with_seg), {}),
         ]
         if train:
             ops += [
                 # affine augmentation - will apply the same affine transformation on each slice
-                (OpRepeat((OpAugSqueeze3Dto2D()), kwargs_per_step_to_add=repeat_images), dict(axis_squeeze=1)),
+                (OpRepeat((OpAugSqueeze3Dto2D()), kwargs_per_step_to_add=repeat_images_with_seg), dict(axis_squeeze=1)),
                 (
                     OpRandApply(
-                        OpSampleAndRepeat(OpAugAffine2D(), kwargs_per_step_to_add=repeat_images),
+                        OpSampleAndRepeat(OpAugAffine2D(), kwargs_per_step_to_add=repeat_images_with_seg),
                         aug_params["apply_aug_prob"],
                     ),
                     dict(
@@ -226,7 +280,7 @@ class PICAI:
                     ),
                 ),
                 (
-                    OpRepeat(OpAugUnsqueeze3DFrom2D(), kwargs_per_step_to_add=repeat_images),
+                    OpRepeat(OpAugUnsqueeze3DFrom2D(), kwargs_per_step_to_add=repeat_images_with_seg),
                     dict(axis_squeeze=1, channels=1),
                 ),
             ]
@@ -268,13 +322,12 @@ class PICAI:
         static_pipeline = PICAI.static_pipeline(
             input_source_gt, paths["data_dir"], paths["seg_dir"], cfg["target"], repeat_images
         )
-        repeat_images_with_seg = repeat_images + [dict(key="data.gt.seg")]
         if train:
             dynamic_pipeline = PICAI.dynamic_pipeline(
-                train=train, repeat_images=repeat_images_with_seg, aug_params=cfg["aug_params"]
+                train=train, repeat_images=repeat_images, aug_params=cfg["aug_params"]
             )
         else:
-            dynamic_pipeline = PICAI.dynamic_pipeline(train=train, repeat_images=repeat_images_with_seg)
+            dynamic_pipeline = PICAI.dynamic_pipeline(train=train, repeat_images=repeat_images)
 
         cacher = SamplesCacher(
             "cache_ver",
