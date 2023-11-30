b62d52fddc0fab083d3a630a7575bf17961f0605
Alex Golts <alex.golts@ibm.com>
2023-11-30 10:16:32 -0500

    add optional mask to pearsonr metric

--- fuse/eval/metrics/libs/stat.py
+++ fuse/eval/metrics/libs/stat.py
@@ -9,15 +9,24 @@ class Stat:
 
     @staticmethod
     def pearson_correlation(
-        pred: Union[np.ndarray, Sequence], target: Union[np.ndarray, Sequence]
+        pred: Union[np.ndarray, Sequence], target: Union[np.ndarray, Sequence], mask: Union[np.ndarray, Sequence, None] = None
     ) -> float:
         """
         Pearson correlation coefficient measuring the linear relationship between two datasets/vectors.
+        :param pred: prediction values
+        :param target: target values
+        :param mask: optional boolean mask. if it is provided, the metric will be applied only to the masked samples
         """
         if isinstance(pred, Sequence):
             pred = np.array(pred)
         if isinstance(target, Sequence):
             target = np.array(target)
+        if isinstance(mask, Sequence):
+            mask = np.array(mask).astype('bool')
+        if mask is not None:
+            pred = pred[mask]
+            target = target[mask]
+
         pred = pred.squeeze()
         target = target.squeeze()
         if len(pred.shape) > 1 or len(target.shape) > 1:
