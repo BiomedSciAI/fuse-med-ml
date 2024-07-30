633cb5e4ec3413af33c7070f7e99cf3f5392fd23
Ben Shapira <ben.shapira@ibm.com>
2024-07-30 10:12:30 -0400

    change from boolean params to crop mode

--- fuse/data/ops/ops_aug_common.py
+++ fuse/data/ops/ops_aug_common.py
@@ -193,29 +193,24 @@ class OpRandCropSeq(OpBase):
         self,
         seed: int,
         crop_max_ratio: float = 0.3,
-        right_only: bool = False,
-        left_only: bool = False,
-        both_sides: bool = True,
+        crop_mode: str = "both",
     ):
         """
         :param crop_max_ratio: The maximum ratio of the string length that can be cropped in total.
                 A random ratio between 0 and crop_max_ratio will be generated for cropping.
+                If crop_max_ratio = 0 the original string will be returned, if crop_max_ratio=1.0 all string will be cropped
 
-        :param both_sides : If True, crop the string from both sides.
-
-        :param left_only : If True, crop the string from the left side only.
-
-        :param right_only : If True, crop the string from the right side only.
+        :param crop_mode: can be one of "both", "right_only", "left_only".
+                            If "both", crop the string from both sides.
+                            If "left_only", crop the string from the left side only.
+                            If "right_only", crop the string from the right side only.
 
         """
         super().__init__()
 
         assert 0 <= crop_max_ratio <= 1.0
-        assert any([both_sides, left_only, right_only])
 
-        self.crop_both_sides = both_sides
-        self.crop_left_only = left_only
-        self.crop_right_only = right_only
+        self.crop_mode = crop_mode
         self.crop_max_ratio = crop_max_ratio
 
         self._random_generator = random.Random(seed)
@@ -226,12 +221,17 @@ class OpRandCropSeq(OpBase):
         string_key_in: str,
         string_key_out: str,
     ) -> dict:
+        """
+        :param string_key_in - key that stores the string to crop, so sample_dict[string_key_in]: str
+        :param string_key_out - key to store the cropped string
+
+        """
         input_string = sample_dict[string_key_in]
         length = len(input_string)
         crop_ratio = self._random_generator.uniform(0, self.crop_max_ratio)
         crop_length = int(length * crop_ratio)
 
-        if self.crop_both_sides:
+        if self.crop_mode == "both":
             left_crop_length = self._random_generator.randint(0, crop_length)
             right_crop_length = crop_length - left_crop_length
             new_string = input_string[
@@ -239,9 +239,9 @@ class OpRandCropSeq(OpBase):
                 if right_crop_length != 0
                 else None
             ]
-        elif self.crop_left_only:
+        elif self.crop_mode == "left_only":
             new_string = input_string[crop_length:]
-        elif self.crop_right_only:
+        elif self.crop_mode == "right_only":
             new_string = input_string[:-crop_length]
         else:
             raise ValueError(
