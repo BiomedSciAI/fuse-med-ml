e3c0be6e3c0b2acd5cef77f0364d13d28d34030f
Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxl010.pok.ibm.com>
2024-09-26 09:59:31 -0400

    adding r2 metric

--- fuse/eval/metrics/regression/metrics.py
+++ fuse/eval/metrics/regression/metrics.py
@@ -2,7 +2,7 @@ from typing import List, Optional, Union
 from fuse.eval.metrics.libs.stat import Stat
 from fuse.eval.metrics.metrics_common import MetricDefault
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 class MetricPearsonCorrelation(MetricDefault):
@@ -103,8 +103,6 @@ class MetricRMSE(MetricDefault):
         See MetricDefault for the missing params
         :param pred: scalar predictions
         :param target: ground truth scalar labels
-        :param threshold: threshold to apply to both pred and target
-        :param balanced: optionally to use balanced accuracy (from sklearn) instead of regular accuracy.
         """
         super().__init__(
             pred=pred,
@@ -129,3 +127,39 @@ class MetricRMSE(MetricDefault):
 
         squared_diff = (pred - target) ** 2
         return squared_diff.mean()
+
+
+class MetricR2(MetricDefault):
+    def __init__(
+        self,
+        pred: str,
+        target: str,
+        **kwargs: dict,
+    ) -> None:
+        """
+        See MetricDefault for the missing params
+        :param pred: scalar predictions
+        :param target: ground truth scalar labels
+        """
+        super().__init__(
+            pred=pred,
+            target=target,
+            metric_func=self.r2,
+            **kwargs,
+        )
+
+    def r2(
+        self,
+        pred: Union[List, np.ndarray],
+        target: Union[List, np.ndarray],
+        **kwargs: dict,
+    ) -> float:
+
+        pred = np.array(pred).flatten()
+        target = np.array(target).flatten()
+
+        assert len(pred) == len(
+            target
+        ), f"Expected pred and target to have the dimensions but found: {len(pred)} elements in pred and {len(target)} in target"
+
+        return r2_score(target, pred)
