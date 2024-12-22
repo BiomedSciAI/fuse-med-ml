d0056565fd6c4ad9ce6fe5ab9436f147baec2d3d
yoels <yoels@login-a.zuvela.ibm.com>
2024-12-22 08:23:05 +0100

    added oplambda easier import

--- fuse/data/__init__.py
+++ fuse/data/__init__.py
@@ -3,7 +3,7 @@ from fuse.data.datasets.dataset_default import DatasetBase, DatasetDefault
 from fuse.data.ops.op_base import OpBase  # DataTypeForTesting,
 from fuse.data.ops.ops_aug_common import OpRandApply, OpSample, OpSampleAndRepeat
 from fuse.data.ops.ops_cast import OpToNumpy, OpToTensor
-from fuse.data.ops.ops_common import OpApplyPatterns, OpFunc, OpKeepKeypaths, OpSet
+from fuse.data.ops.ops_common import OpApplyPatterns, OpFunc, OpKeepKeypaths, OpSet, OpLambda
 from fuse.data.ops.ops_read import OpReadDataframe
 from fuse.data.pipelines.pipeline_default import PipelineDefault
 from fuse.data.utils.collates import CollateDefault
